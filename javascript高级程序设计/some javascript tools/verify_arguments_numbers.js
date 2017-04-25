6f3af0ee5c4e420810468612fca0bb92feb92d23
jose <jose.xiaohang@gmail.com>
2017-04-25 20:48:51 +0800

    new

--- javascript高级程序设计/some javascript tools/verify_arguments_numbers.js
+++ javascript高级程序设计/some javascript tools/verify_arguments_numbers.js
@@ -1,3 +1,8 @@
 function verifyArguments() {
     alert(arguments.lenth);
 }; //构造函数，验证参数数量
+
+
+console.log(person instanceof Object);
+
+//instanceof操作符，用来检查变量是不是基本数据类型。它可以检查对象，如果一个变量是基本类型的话，返回false。是引用类型的话，返回true
